a6f34d7246dceff0991061ccaee425c18a748714
Precious Okwukwe Amaechi <poamaechi@outlook.com>
2022-07-01 03:16:19 -0700

    low level programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
 
-
-
 /**
- *
  * * main - prints the size of various types
  *
  * * Return: 0 if exited properly, non-zero otherwise
  *
- * */
+ *
+ */
 
 int main(void)
 
 {
-
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
 
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
