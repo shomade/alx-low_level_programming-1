b1b877a87fbb74fecc16dce949618d666d673ffe
Precious Okwukwe Amaechi <poamaechi@outlook.com>
2022-07-06 16:05:43 -0700

    functions_nested_loops

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,13 +11,13 @@ int main(void)
 	n1 = 1;
 	n2 = 2;
 	boolean = boolean2 = 1;
-	printf("%1d, %1d", n1, 'n2');
+	printf("%1d, %1d"," n1", "n2");
 	for (c = 0; c < 96; c++)
 {
 	if (boolean)
 {
 	fn = n1 + n2;
-	printf(", %1d", 'fn');
+	printf(", %1d", "fn");
 	n1 = n2;
 	n2 = fn;
 }
