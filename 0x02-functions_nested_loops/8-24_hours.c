d06fce5e9e7f1697948a326abf107e40e09301a8
Precious Okwukwe Amaechi <poamaechi@outlook.com>
2022-07-06 09:37:55 +0100

    functions_nested_loops

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,7 +9,7 @@ void jack_bauer(void)
 	for (i = 0; i < 24; i++);
 	{
 	for (j = 0; j < 60;  j++);
-		{
+	{
 			if (i < 10)
 			{
 			_putchar('0');
@@ -33,6 +33,6 @@ void jack_bauer(void)
 			_putchar((j % 10) + '0');
 			}
 			_putchar('\n');
-		}
+	}
 	}
 }
