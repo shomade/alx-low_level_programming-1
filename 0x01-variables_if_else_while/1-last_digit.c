df37f44e1768a8f7b00182b4fc58c97e97eb8c3f
Precious Okwukwe Amaechi <poamaechi@outlook.com>
2022-07-06 01:46:44 -0700

    variables

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,6 +10,7 @@
  *    *Return: Always 0 (Success)
  *
  */
+
 int main(void)
 
 {
@@ -17,8 +18,8 @@ int main(void)
 	int x;
 	
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
 
+	n = rand() - RAND_MAX / 2;
 	x = n % 10;
 	printf("Last digit of %d is %d ", n, x);
 	if (x > 5)
@@ -33,8 +34,8 @@ int main(void)
 	{
 		printf("and is less than 6 and not 0");
 	}
-
+	
 	printf("\n");
-
+	
 	return (0);
 }
