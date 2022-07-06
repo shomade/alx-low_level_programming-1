9924f0b335c853a3c73858c9a725598bdb78fde9
Precious Okwukwe Amaechi <poamaechi@outlook.com>
2022-07-06 09:14:11 +0100

    functions_nested_loops

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,13 +4,13 @@
  */
 void jack_bauer(void)
 {
-	int i, j:
+	int i, j;
 
-	for (i = 0; i < 24; i++)
+	for (i = 0; i < 24; i++);
 	{
-		for (j = 0; j < 60;  j++)
+		for (j = 0; j < 60;  j++);
 		{
-			if (i <10)
+			if (i < 10)
 			{
 				_putchar('0');
 				_putchar(i + '0');
