b7934970b06ba86d66e21a85f8f504a0f16e9969
Precious Okwukwe Amaechi <poamaechi@outlook.com>
2022-07-06 21:08:24 -0700

    functions_nested_loops

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -12,19 +12,20 @@ int main(void)
 	j = 1;
 
 	k = 2;
-
+	
 	for (i = 1; i <= 50; ++i)
-{
-	if (j != 20365011074)
-{
-	printf("%1d, ", j);
-}	else
-{
-	printf("%1d\n", j);
-}
-	next = j + k;
-	j = k;
-	k = next;
-}
+	{
+	    if (j != 20365011074)
+	    {
+		    printf("%1d, ", j);
+	    } else
+	    {
+		    printf("%1d\n", j);
+	    }
+	    next = j + k;
+	    j = k;
+	    k = next;
+	}
+
 	return (0);
 }
