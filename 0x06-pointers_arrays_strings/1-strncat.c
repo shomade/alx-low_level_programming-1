2298b26ec7c0722425c1069b8978c60217061255
Precious Okwukwe Amaechi <poamaechi@outlook.com>
2022-07-14 05:23:30 -0700

    M P A S

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,29 +1,27 @@
 #include "main.h"
-
 /**
- * _strcat - concatenates two strings,
+ * _strncat - concatenates two strings,
  * @dest: destination.
  * @src: source.
+ * @n: amount of bytes used from src.
  * Return: the pointer to dest.
  */
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
-	int count = 0, count2 = 0;
+	int count1 = 0, count2 = 0;
 
-	while (*(dest + count) != '\0')
+	while (*(dest + count1) != '\0')
 	{
-		count++;
+		count1++;
 	}
 
-	while (count2 >= 0)
+	while (count2 < n)
 	{
-		*(dest + count) = *(src + count2);
+		*(dest + count1) = *(src + count2);
 		if (*(src + count2) == '\0')
 			break;
-		count++;
+		count1++;
 		count2++;
 	}
 	return (dest);
-
 }
-
