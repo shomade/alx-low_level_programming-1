b41dbde7f42a90b3f313abc2001743b0cf15e5cd
Precious Okwukwe Amaechi <poamaechi@outlook.com>
2022-08-11 17:13:42 -0700

    Singly

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * premain - run before main
+#include <stdio.h>
+
+/**
+ * premain - run before main
+ */
+void __attribute__ ((constructor)) premain()
+{
+printf(
+"You're beat! and yet, you must allow,\nI bore my house upon my back!\n"
+);
+}
