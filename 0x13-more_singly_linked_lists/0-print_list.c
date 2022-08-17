67a63995583156a9630ef86180cd6a5b6035f650
Precious Okwukwe Amaechi <poamaechi@outlook.com>
2022-08-17 06:30:33 +0100

    Updated

--- 0x13-more_singly_linked_lists/0-print_list.c
+++ 0x13-more_singly_linked_lists/0-print_list.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all the elements of a list_t list.
- * @h: singly linked list.
- * Return: number of elements in the list.
- */
+  * print_listint - prints all the elements of a list_t list.
+  * @h: singly linked list.
+  * Return: number of elements in the list.
+  */
 size_t print_listint(const listint_t *h)
 {
 	size_t n = 0;
