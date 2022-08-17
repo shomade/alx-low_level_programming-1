bb0393a51bb0d4bf6f2376657d6772ad42390b65
Precious Okwukwe Amaechi <poamaechi@outlook.com>
2022-08-16 22:41:02 -0700

    Singly

--- 0x13-more_singly_linked_lists/0-print_list.c
+++ 0x13-more_singly_linked_lists/0-print_list.c
@@ -1,19 +1,20 @@
 #include "lists.h"
 
 /**
-  * print_listint - prints all the elements of a list_t list.
-  * @h: singly linked list.
-  * Return: number of elements in the list.
-  */
+ *  * print_listint - prints all the elements of a list_t list.
+ *   * @h: singly linked list.
+ *    * Return: number of elements in the list.
+ *     */
 size_t print_listint(const listint_t *h)
 {
-	size_t n = 0;
+		size_t n = 0;
 
-	while (h != NULL)
-	{
-		printf("%i\n", h->n);
-		h = h->next;
-		n++;
-	}
-	return (n);
+			while (h != NULL)
+					{
+								printf("%i\n", h->n);
+										h = h->next;
+												n++;
+													}
+
+				return (n);
 }
